61b85323436f2dfcf301e065515bbc55995ddc91
Maxwell Estes <max@esharesinc.com>
2017-01-28 17:57:15 -0800

    a test

--- gitscreen.py
+++ gitscreen.py
@@ -1,8 +1,14 @@
 import web
 
-urls = ('/.*', 'hooks')
+urls = ('/.*', 'hooks',
+        '/', 'index')
 
 app = web.application(urls, globals())
+
+class index:
+    def GET(self):
+        # redirect to the static file ...
+        raise web.seeother('/index.html')
 
 class hooks:
     def POST(self):
